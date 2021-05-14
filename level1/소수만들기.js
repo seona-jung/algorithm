729d62f81522af6b5bb512eb278ebfdf4e290e2f
seona-jung <seona.jung.job@gmail.com>
2021-05-14 16:45:14 +0900

    2021.05.14.

--- level1/소수만들기.js
+++ level1/소수만들기.js
@@ -1,5 +1,3 @@
-// 선아
-
 function solution(nums) {
   const isPrime = function (num) {
     if (num === 1) return false;
@@ -19,4 +17,32 @@ function solution(nums) {
     }
   }
   return count;
+}
+
+// 같이 알고리즘 공부 스터디하는 친구가 푼 방법
+// 아주 great.. 재귀를 암만 해도 이렇게 못해..ㅠㅠ
+
+function solution(nums) {
+  let primes = 0;
+
+  function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) return false;
+    }
+    return n > 1;
+  }
+
+  function findPrime(i, sum, count) {
+    if (count === 3) {
+      if (isPrime(sum)) primes++;
+      return;
+    }
+    if (i === nums.length) return;
+    findPrime(i + 1, sum + nums[i], count + 1);
+    findPrime(i + 1, sum, count);
+  }
+
+  findPrime(0, 0, 0);
+
+  return primes;
 }
