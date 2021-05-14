e9dbec189c34fe143ff6d9cd467588bc69a72bde
seona-jung <seona.jung.job@gmail.com>
2021-05-14 16:37:21 +0900

    2021.05.14.

---
+++ level1/소수만들기.js
@@ -0,0 +1,22 @@
+// 선아
+
+function solution(nums) {
+  const isPrime = function (num) {
+    if (num === 1) return false;
+    if (num === 2) return true;
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) count++;
+      }
+    }
+  }
+  return count;
+}
