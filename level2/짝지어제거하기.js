2e000d00734674ffc48c8f54bb2aed8db7753e8d
seona-jung <seona.jung.job@gmail.com>
2021-06-18 19:43:00 +0900

    2021.06.18.

---
+++ level2/짝지어제거하기.js
@@ -0,0 +1,21 @@
+// 효율성 통과 못하는 내 답안..
+function solution(s) {
+  // 앞에서부터 2개씩 짝 지어 보면서 두 개가 같으면 삭제.
+  // 그리고 처음부터 다시 반복문 시작
+  // 반복문 돌 동안 2개 짝이 하나도 없었다면 그제서야 이 반복문 종료
+  if (s.length % 2 !== 0) return 0;
+
+  while (true) {
+    let keepGoing = false;
+    for (let i = 0; i < s.length - 1; i++) {
+      if (s[i] === s[i + 1]) {
+        s = s.slice(0, i) + s.slice(i + 2);
+        keepGoing = true;
+        break;
+      }
+    }
+    if (!keepGoing) break;
+  }
+
+  return s.length === 0 ? 1 : 0;
+}
